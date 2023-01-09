d9554c00d0a1a4c0b845b6ae9295ba0ce5f50760
“Gigi <debortolg@gmail.com>
2023-01-08 23:48:50 -0800

    added date and time to top of the html page with moment.js

---
+++ assets/js/index.js
@@ -0,0 +1,21 @@
+// Current date and time + hour using moment.js documentation
+let rightNow = moment().format("dddd, MMMM Do YYYY, h:mm a");
+let currentHour = moment().format("HH:mm a");
+// Display current date and time at the top of the page in the <p> element using jquery 
+$('#currentDay').text(rightNow); 
+
+
+// past hour - all need to be color coded
+
+// present hour
+
+// future time
+
+// When click into time block an event can be entered 
+
+// saved to local storage after SAVE button is clicked
+
+// when page is refreshed the saved events stay on the page
+
+
+
